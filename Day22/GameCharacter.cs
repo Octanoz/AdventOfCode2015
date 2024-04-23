35ff5bbf3c338090589e52ba8dab8390ed4b2857
Octanoz <130052848+Octanoz@users.noreply.github.com>
2024-04-23 11:12:08 +0200

    Minor refactor day 22 GameCharacter

--- Day22/GameCharacter.cs
+++ Day22/GameCharacter.cs
@@ -2,6 +2,11 @@ namespace Day22;
 
 class GameCharacter
 {
+    public int Hitpoints { get; set; }
+    public int Mana { get; set; }
+    public int Damage { get; set; }
+    public int Armour { get; set; }
+
     public GameCharacter(int hitpoints, int mana = 0, int damage = 0, int armour = 0)
     {
         Hitpoints = hitpoints;
@@ -9,9 +14,4 @@ class GameCharacter
         Damage = damage;
         Armour = armour;
     }
-
-    public int Hitpoints { get; set; }
-    public int Mana { get; set; }
-    public int Damage { get; set; }
-    public int Armour { get; set; }
 }
